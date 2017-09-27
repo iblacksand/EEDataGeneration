e7ebce978e7ceb60c326340a8edb71bba8808d25
John Elizarraras <chess4j@gmail.com>
2017-09-27 12:34:22 -0600

    first commit

---
+++ DataGeneration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("all-data.csv");
+            string[][] data = new string[lines.Length][];
+            for(int i = 0; i < lines.Length; i++){
+                data[i] = new string[]{lines[i].Split(',')[0].Trim(), lines[i].Split(',')[1].Trim()};
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
