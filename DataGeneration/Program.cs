06995751db1cbf4deccf49815a460911707df7f4
John Elizarraras <chess4j@gmail.com>
2017-10-08 11:30:37 -0600

    adds quadratic's first step(really slow)

--- DataGeneration/Program.cs
+++ DataGeneration/Program.cs
@@ -15,10 +15,12 @@ namespace DataGeneration
         {
             int response = 0;
             LGetResponse:
-            string choices = "Actions:\n1. Generate New Fake and Real Data\n2. Generate Encrypted Data\n3. Decrypt Data";
+            string choices = "Actions:\n1. Generate New Fake and Real Data\n2. Generate Encrypted Data\n3. Decrypt Data\n4. Linear Test\n5. Quadratic Test";
             Console.WriteLine(choices);
             Int32.TryParse(Prompt("Choose an action.(EX: 1)"), out response);
             switch(response){
+                case -1:
+                    break;
                 case 1:
                     GenerateData(Prompt("What is the file for the data"),Double.Parse(Prompt("What percentage of data is fake?(.1 or .25 or ..)")));
                     break;
@@ -30,6 +32,10 @@ namespace DataGeneration
                     break;
                 case 4:
                     SpeedTest();
+                    break;
+                case 5:
+                    Quadratic();
+                    Console.WriteLine("Done");
                     break;
                 default:
                     Console.WriteLine("Invalid Choice");
@@ -113,8 +119,8 @@ namespace DataGeneration
                     }
                 }
                 watch.Stop();
-                DeleteFile("Info.txt");
-                File.WriteAllText("Info.txt","Time for Encryption: " + watch.ElapsedMilliseconds + "ms");
+                // DeleteFile("Info.txt");
+                File.AppendAllText("Info.txt","\nTime for Encryption: " + watch.ElapsedMilliseconds + "ms");
                 Console.WriteLine("Finished Encrypting");
                 return val;
             }
@@ -154,14 +160,21 @@ namespace DataGeneration
             string[] total = File.ReadAllLines("AllData.txt");
             string[] fakes = File.ReadAllLines("FakeData.txt");
             string[] reals = File.ReadAllLines("RealData.txt");
-            List<string> corrected = new List<string>(reals);
+            string[] corrected = new string[total.Length];
             Stopwatch watch = new Stopwatch();
             Console.WriteLine("Building total array");
             watch.Start();
-            for(int i = 0; i < fakes.Length; i++){
-                corrected.Insert(i * variation, fakes[i]);
+            int f = 0;
+            int r = 0;
+            for(int i = 0; i < total.Length/variation; i++){
+                corrected[variation * i] = fakes[f];
+                f++;
+                for(int j = 1; j < variation; j++){
+                    corrected[variation * i + j] = reals[r];
+                    r++;
+                }
             }
-            if(corrected.Count!= total.Length) Console.WriteLine("ERROR NOT RIGHT BRO");
+            if(corrected.Length!= total.Length) Console.WriteLine("ERROR NOT RIGHT BRO");
             watch.Stop();
             File.AppendAllText("Info.txt","\nTime for Linear Encryption: " + watch.ElapsedMilliseconds + "ms");
             Stopwatch swatch = new Stopwatch();
@@ -169,7 +182,7 @@ namespace DataGeneration
             string[] real = new string[reals.Length];
             swatch.Start();
             int c = 0;
-            for(int i = 0; i < corrected.Count; i++){
+            for(int i = 0; i < corrected.Length; i++){
                 if(i % variation != 0){
                     real[c] = corrected[i];
                     c++;
@@ -178,6 +191,103 @@ namespace DataGeneration
             swatch.Stop();
             File.AppendAllText("Info.txt","\nTime for Linear Decryption: " + swatch.ElapsedMilliseconds + "ms");
             File.WriteAllLines("LinearReals.txt", real);
+            Console.WriteLine("Finished");
+        }
+
+        public static int SafeIndex(int[] indexes, int index, int max){
+            if(indexes.Contains(index)){
+                return SafeIndex(indexes, (index + 1) % max, max);
+            }
+            return index;            
+        }
+
+        public static void Quadratic(){
+            string[] total = File.ReadAllLines("AllData.txt");
+            string[] fakes = File.ReadAllLines("FakeData.txt");
+            string[] reals = File.ReadAllLines("RealData.txt");
+            string[] corrected = new string[total.Length];
+            int[] indexes = new int[fakes.Length];
+            Stopwatch eWatch = new Stopwatch();
+            int seed = Int32.Parse(Prompt("What seed do you want? (32 bit int only)"));
+            eWatch.Start();
+            // Console.Write(indexes[1]);
+            Random rand = new Random(seed);
+            QuadFunction fun = new QuadFunction(rand.Next(1000), rand.Next(1000), rand.Next(1000));
+            // Conso/le.WriteLine("Fakes: " + fakes.Length + "\nIndexes:" + indexes.Length);
+            for(int i = 0; i < fakes.Length; i++){
+                int index = SafeIndex(indexes,Math.Abs(fun.Evaluate(i * seed) % total.Length), total.Length);
+                // Console.WriteLine(index);
+                corrected[index] = fakes[i];
+                // Console.WriteLine(i);
+                indexes[i] = index;
+            }
+            int r = 0;
+            for (int i = 0; i < total.Length; i++)
+            {
+                if(r >= reals.Length){
+                    Console.Write("ERRORORRORORORORO\n");
+                    break;
+                }
+                if(!indexes.Contains(i)){
+                    corrected[i] = reals[r];
+                    r++;
+                }
+            }
+            eWatch.Stop();
+             File.AppendAllText("Info.txt","\nTime for Quad Encryption: " + eWatch.ElapsedMilliseconds + "ms");
+            bool dupsFound = false;
+            
+            for(int i = 0; i < total.Length; i++){
+                if(corrected[i] == null){
+                    dupsFound = true;
+                    break;
+                }
+                if(corrected[i].Trim().Equals("")){
+                    dupsFound = true;
+                    break;
+                }
+            }
+            if(dupsFound) Console.WriteLine("FOUND DUPLICATES");
+            Stopwatch dWatch = new Stopwatch();
+            dWatch.Start();
+            string[] eReals = new string[reals.Length];
+             for(int i = 0; i < fakes.Length; i++){
+                int index = SafeIndex(indexes,Math.Abs(fun.Evaluate(i * seed) % total.Length), total.Length);
+                corrected[index] = "";
+            }
+            int rr = 0;
+
+            for(int i = 0; i < total.Length; i++){
+                if(rr >= eReals.Length) break;
+                if(corrected[i] != null && !corrected[i].Trim().Equals("")){
+                    eReals[rr] = corrected[i];
+                    rr++; 
+                }
+            }
+            dWatch.Stop();
+            File.AppendAllText("Info.txt","\nTime for Quad Decryption: " + dWatch.ElapsedMilliseconds + "ms");
+            File.WriteAllLines("QuadReals.txt", eReals);
+            Console.WriteLine("Finished");
+        }
+
+        public static void Shifty(){
+
+        }
+    }
+
+    public class QuadFunction{
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        public QuadFunction(int a, int b, int c){
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public int Evaluate(int x){
+            return (a * (x * x)) + (b * x) + c;
         }
     }
 }
